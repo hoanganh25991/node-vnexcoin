5f58d3be6ee5e00f110a91b49e4dc02704712b57
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-12-29 22:54:16 +0700

    Update decode/encode test

--- src/api/encryptPayload.js
+++ src/api/encryptPayload.js
@@ -6,24 +6,24 @@ const { ENCRYPT_PAYLOAD_KEY: key, ENCRYPT_PAYLOAD_ALGORITHM: algorithm } = proce
 const _ = console.log
 
 // By default, cipher hash MD5 on key
-const encrypt = (str, key) => {
+const encryptAesBase64 = ({ str, algorithm, key }) => {
   const cipher = crypto.createCipher(algorithm, key)
   cipher.update(str, "utf8", "base64")
   return cipher.final("base64")
 }
 
-const decrypt = (str, key) => {
+const decryptAesBase64 = ({ str, algorithm, key }) => {
   const decipher = crypto.createDecipher(algorithm, key)
   decipher.update(str, "base64", "utf8")
   return decipher.final("utf8")
 }
 
 export const decode = str => {
-  const payloadStr = decrypt(str, key)
+  const payloadStr = decryptAesBase64({ str, algorithm, key })
   return JSON.parse(payloadStr)
 }
 
 export const encode = obj => {
   const str = JSON.stringify(obj)
-  return encrypt(str, key)
+  return encryptAesBase64({ str, algorithm, key })
 }
