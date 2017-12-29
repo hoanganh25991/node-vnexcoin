7c26de93d55d58956127a48fac4b0b3c4b28bc97
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-12-29 22:18:34 +0700

    Check done msg

---
+++ src/vcb-sms/isDoneDepositMsg.js
@@ -0,0 +1,2 @@
+export const DONE_DEPOSIT_MSG = "CK XONG"
+export const isDoneDepositMsg = msg => msg === DONE_DEPOSIT_MSG
---
+++ src/vcb-sms/isReceivedCoin.js
@@ -0,0 +1,2 @@
+export const RECEIVED_COIN_MSG = "DONE"
+export const isReceivedCoinMsg = msg => msg === RECEIVED_COIN_MSG
