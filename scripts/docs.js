b3a38b677cde2e0e67b607f44e6fa7f6410ea527
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-12-30 11:06:50 +0700

    Add firebase for FCM

---
+++ scripts/docs.js
@@ -0,0 +1,7 @@
+import cpr from "child_process"
+
+const _ = console.log
+const cmd = `postmanerator -output=tmp/doc.html -collection=tmp/vnexcoin.postman_collection.json`
+
+_("[INFO] Generate docs in tmp folder")
+_(cpr(cmd).toString())
