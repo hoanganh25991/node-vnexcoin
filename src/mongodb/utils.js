dc210b06a6b8a127fbd067dd44010e763c6a6484
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-12-30 16:44:16 +0700

    Fix bugs, schem default options

--- src/mongodb/utils.js
+++ src/mongodb/utils.js
@@ -2,6 +2,7 @@
  * adjust toJSON transformation
  */
 export const transformEnhance = schema => {
+  if (!schema.options.toObject) schema.options.toObject = {}
   schema.options.toObject.transform = function(doc, ret, options) {
     ret.id = ret._id
     delete ret._id
@@ -9,6 +10,7 @@ export const transformEnhance = schema => {
     return ret
   }
 
+  if (!schema.options.toJSON) schema.options.toJSON = {}
   schema.options.toJSON.transform = function(doc, ret, options) {
     ret.id = ret._id
     delete ret._id
